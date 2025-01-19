e741f43125a828a1cdb80ee31bc3423272713c2c
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-01-19 22:16:14 +0000

    Completed practice 3

--- practice-3.sql
+++ practice-3.sql
@@ -13,3 +13,22 @@
 -- | Beverly                |
 
 
+SELECT DISTINCT 
+--id 
+--url 
+-- ,name 
+-- ,body 
+-- ,host_name 
+-- ,host_since 
+neighborhood 
+-- ,property_type 
+-- ,accommodates 
+-- ,bathrooms 
+-- ,bedrooms 
+-- ,price 
+-- ,minimum_nights 
+-- ,maximum_nights 
+-- ,available 
+
+FROM listings
+ORDER BY 1
