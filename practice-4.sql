556c36747c4e951ac19a49da08f21082a0b03f2e
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-01-19 22:18:06 +0000

    Completed practice 4

--- practice-4.sql
+++ practice-4.sql
@@ -5,3 +5,23 @@
 -- +----------+
 
 
+SELECT COUNT(*) 
+--id 
+--url 
+-- ,name 
+-- ,body 
+-- ,host_name 
+-- ,host_since 
+--,neighborhood 
+-- ,property_type 
+-- ,accommodates 
+-- ,bathrooms 
+-- ,bedrooms 
+-- ,price 
+-- ,minimum_nights 
+-- ,maximum_nights 
+-- ,available 
+
+FROM listings
+WHERE 1=1
+AND neighborhood = 'Lincoln Park'
